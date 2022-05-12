8164d7c1fd994ad9e20f02eca6842b12d1b0f23f
rusanov-ao <rusanov-ao@inbox.ru>
2022-05-12 21:15:57 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Server started");
+        int port = 8089;
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket clientSocket = serverSocket.accept(); // подключаемся
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        System.out.println("New connection accepted");
+        final String name = in.readLine();
+        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+    }
+}
